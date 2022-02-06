ce3e099e4a66c3a5f8c99af53b63f9d4b2170acc
Namjuhee <zjql7312@naver.com>
2022-02-06 23:05:46 +0900

    +공부내용

--- codingTest_0206/src/codingTest_0206/B11478.java
+++ codingTest_0206/src/codingTest_0206/B11478.java
@@ -2,9 +2,11 @@ package codingTest_0206;
 import java.util.*;
 
 //부분 문자열을 저장할 자료구조를 잘 선택하지 않으면 시간 초과로 오답이 나오는 문제입니다.
-//ArrayList를 이용할 경우 시간 초과가 발생하여 HashSet을 이용해야 한다는 점만 유의하면 될 것 같습니다.
+// ArrayList를 이용할 경우 시간 초과가 발생하여 HashSet을 이용해야 한다는 점만 유의하면 될 것 같습니다.
 
-//출처: https://airzinc.tistory.com/entry/백준-11478JAVA [박상어 블로그]
+// 중복제거 할 떄 HashSet / 중복제거&정렬 TreeSet
+// ArrayList 검색많음 ArrayList / 삽입삭제많음 LinkedList
+// 출처: https://airzinc.tistory.com/entry/백준-11478JAVA [박상어 블로그]
 public class B11478 {
 	public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
