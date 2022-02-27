fe03c8cd998ad3350259f0cdb70465703111f3a1
Namjuhee <zjql7312@naver.com>
2022-02-27 22:19:08 +0900

    +실행

--- codingTest_2027/src/codingTest_2027/Algorithm_program.java
+++ codingTest_2027/src/codingTest_2027/Algorithm_program.java
@@ -3,6 +3,20 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/*
+테스트 1 〉	통과 (0.19ms, 73.2MB)
+테스트 2 〉	통과 (0.21ms, 72.1MB)
+테스트 3 〉	통과 (0.25ms, 72.8MB)
+테스트 4 〉	통과 (0.19ms, 65.8MB)
+테스트 5 〉	통과 (0.17ms, 77.5MB)
+테스트 6 〉	통과 (0.11ms, 76.4MB)
+테스트 7 〉	통과 (0.17ms, 72.1MB)
+테스트 8 〉	통과 (0.14ms, 73.3MB)
+테스트 9 〉	통과 (0.18ms, 72.9MB)
+테스트 10 〉	통과 (0.26ms, 76.2MB)
+테스트 11 〉	통과 (0.13ms, 77MB)
+*/
+
 public class Algorithm_program {
 	public int[] solution(int[] progresses, int[] speeds){
         Queue<Integer> queue = new LinkedList<>();
